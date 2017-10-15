b638186145aa1a08cef84d7cf2118b21f1032a14
Guangwei WANG <wgw0415@gmail.com>
2017-10-15 18:31:36 +0800

    feat: clear training data

--- code/img_flip.py
+++ code/img_flip.py
@@ -4,16 +4,20 @@ from scipy import misc
 import numpy as np
 
 def flip_and_save_images(img_dir, extension):
-  os.chdir(img_dir)
-  files = glob.glob("*." + extension)
-  for i, file in enumerate(files):
-    print(i)
-    img = misc.imread(file, flatten=False, mode='RGB')
-    flipped_img = np.fliplr(img)
-    misc.imsave("flipped" + file, flipped_img)
+    os.chdir(img_dir)
+    files = glob.glob("*." + extension)
+    
+    total_num = 0
+    for i, file in enumerate(files):
+        img = misc.imread(file, flatten=False, mode='RGB')
+        flipped_img = np.fliplr(img)
+        misc.imsave("flipped" + file, flipped_img)
+        total_num += 1
+       
+    print(total_num, ' files have been flipped.')
 
-train_mask_dir = "../data/train/masks/"
+# train_mask_dir = "../data/train/masks/"
+# flip_and_save_images(train_mask_dir, "png")
+
 train_images_dir = "../data/train/images/"
-
-flip_and_save_images(train_mask_dir, "png")
 flip_and_save_images(train_images_dir, "jpeg")
